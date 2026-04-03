47d0591d201aecb3a690984a5d23b8112bca432e
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-04 01:01:14 +0300

    chore: add nest base configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CommentsModule } from './comments/comments.module';
+import configuration from './common/config/configuration';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ load: [configuration] }),
+		ArticlesModule,
+		CategoriesModule,
+		UsersModule,
+		CommentsModule,
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(4000);
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+		}),
+	);
+
+	const configService = app.get(ConfigService);
+
+	const apiPrefix = configService.get<string>('apiPrefix');
+	if (apiPrefix) app.setGlobalPrefix(apiPrefix);
+
+	const port = configService.get<number>('port');
+
+	if (!port) throw new Error('PORT environment variable is missing');
+
+	await app.listen(port);
+
+	const logger = new Logger('Bootstrap');
+
+	logger.debug(`\n  > Application is running on: http://localhost:${port}/${apiPrefix}`);
 }
 bootstrap();
