52126c8202af19eed134cea9d290589600152df7
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-05 13:26:32 +0300

    feat: add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { ArticlesModule } from './articles/articles.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CommentsModule } from './comments/comments.module';
 import configuration from './common/config/configuration';
+import { LoggerMiddleware } from './common/middleware/logger-middleware..middleware';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -16,4 +17,8 @@ import { UsersModule } from './users/users.module';
 		CommentsModule,
 	],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes('*');
+	}
+}
---
+++ src/common/middleware/logger-middleware..middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+	private readonly logger = new Logger(LoggerMiddleware.name);
+
+	use(req: Request, res: Response, next: NextFunction) {
+		res.on('finish', () => {
+			const { method, originalUrl } = req;
+
+			this.logger.log(`[${method}]${originalUrl}`);
+		});
+
+		next();
+	}
+}
