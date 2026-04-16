734b8c546507e69a422c646e19869eae181b4620
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-16 15:51:12 +0300

    chore: update drizzle migrations

--- drizzle/20260416124651_init/migration.sql
+++ drizzle/20260416124651_init/migration.sql
@@ -1,9 +1,9 @@
 CREATE TYPE "ArticleStatus" AS ENUM('DRAFT', 'PUBLISHED', 'ARCHIVED');--> statement-breakpoint
 CREATE TYPE "UserRole" AS ENUM('ADMIN', 'EDITOR', 'VIEWER');--> statement-breakpoint
 CREATE TABLE "article_to_tags" (
-	"A" uuid,
-	"B" uuid,
-	CONSTRAINT "article_to_tags_pkey" PRIMARY KEY("A","B")
+	"article_id" uuid,
+	"tag_id" uuid,
+	CONSTRAINT "article_to_tags_pkey" PRIMARY KEY("article_id","tag_id")
 );
 --> statement-breakpoint
 CREATE TABLE "articles" (
@@ -47,8 +47,8 @@ CREATE TABLE "users" (
 --> statement-breakpoint
 CREATE INDEX "status_idx" ON "articles" ("status");--> statement-breakpoint
 CREATE INDEX "category_id_idx" ON "articles" ("category_id");--> statement-breakpoint
-ALTER TABLE "article_to_tags" ADD CONSTRAINT "article_to_tags_A_articles_id_fkey" FOREIGN KEY ("A") REFERENCES "articles"("id") ON DELETE CASCADE;--> statement-breakpoint
-ALTER TABLE "article_to_tags" ADD CONSTRAINT "article_to_tags_B_tags_id_fkey" FOREIGN KEY ("B") REFERENCES "tags"("id") ON DELETE CASCADE;--> statement-breakpoint
+ALTER TABLE "article_to_tags" ADD CONSTRAINT "article_to_tags_article_id_articles_id_fkey" FOREIGN KEY ("article_id") REFERENCES "articles"("id") ON DELETE CASCADE;--> statement-breakpoint
+ALTER TABLE "article_to_tags" ADD CONSTRAINT "article_to_tags_tag_id_tags_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "tags"("id") ON DELETE CASCADE;--> statement-breakpoint
 ALTER TABLE "articles" ADD CONSTRAINT "articles_author_id_users_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE SET NULL;--> statement-breakpoint
 ALTER TABLE "articles" ADD CONSTRAINT "articles_category_id_categories_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL;--> statement-breakpoint
 ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_users_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE;--> statement-breakpoint
