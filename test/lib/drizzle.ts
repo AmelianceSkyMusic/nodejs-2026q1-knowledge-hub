95b28859826bb85c95d8aca2465321a52674e7d4
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-19 12:59:03 +0300

    test: update exports

--- test/lib/drizzle.ts
+++ test/lib/drizzle.ts
@@ -5,10 +5,8 @@ import { Pool } from 'pg';
 import { relations } from '../../src/drizzle/db/relations';
 import * as schema from '../../src/drizzle/db/schema';
 
-const pool = new Pool({
+export const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 });
 
-const drizzle = drizzleDb({ client: pool, schema, relations, casing: 'snake_case' });
-
-export default drizzle;
+export const drizzle = drizzleDb({ client: pool, schema, relations, casing: 'snake_case' });
