c6b2ec0e976b7fa37577dfbef79740c3a64a3f7c
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-24 15:14:35 +0300

    refactor: add CustomZodValidationPipe custom error log

--- src/common/pipes/custom-zod-validation.pipe.ts
+++ src/common/pipes/custom-zod-validation.pipe.ts
@@ -1,11 +1,34 @@
-import { createZodValidationPipe, ZodValidationException } from 'nestjs-zod';
+import { ArgumentMetadata, Injectable, Logger } from '@nestjs/common';
+import {
+	createZodValidationPipe,
+	ZodSchemaDeclarationException,
+	ZodValidationException,
+} from 'nestjs-zod';
 
 import type { ZodError } from 'zod';
 
-export const CustomZodValidationPipe = createZodValidationPipe({
+const BaseCustomZodValidationPipe = createZodValidationPipe({
 	strictSchemaDeclaration: process.env.NODE_ENV !== 'production',
 
 	createValidationException: (error: ZodError) => {
 		return new ZodValidationException(error);
 	},
 });
+
+@Injectable()
+export class CustomZodValidationPipe extends BaseCustomZodValidationPipe {
+	private readonly logger = new Logger(CustomZodValidationPipe.name);
+
+	override transform(value: unknown, metadata: ArgumentMetadata) {
+		try {
+			return super.transform(value, metadata);
+		} catch (error) {
+			if (error instanceof ZodSchemaDeclarationException) {
+				this.logger.error(
+					`Zod Schema Declaration Error: ${error.message}. This is likely due to missing @ZodDto() in your controller route`,
+				);
+			}
+			throw error;
+		}
+	}
+}
