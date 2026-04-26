5ee10247ad5af9d3039718bbf1ec37ad073fcc31
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-26 13:55:14 +0300

    refactor: update auth guard move check path logic to utils

--- src/common/utils/guard.ts
+++ src/common/utils/guard.ts
@@ -3,9 +3,19 @@ import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 import type { ExecutionContext } from '@nestjs/common';
 import type { Reflector } from '@nestjs/core';
 
-export const checkIsPublic = (reflector: Reflector, context: ExecutionContext): boolean => {
+export function checkIsPublic(reflector: Reflector, context: ExecutionContext): boolean {
 	return reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
 		context.getHandler(),
 		context.getClass(),
 	]);
-};
+}
+
+const PUBLIC_PATHS = ['/', '/doc'];
+
+export function checkIsSystemPublicPath(url: string) {
+	if (!url) return false;
+	return PUBLIC_PATHS.some((path) => {
+		if (path === '/') return url === '/';
+		return url.startsWith(path);
+	});
+}
