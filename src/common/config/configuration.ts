939c1b0cec8d7fc0571a9fe9c3dae182a30d10b3
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-04-26 23:15:00 +0300

    chore: update configuration

--- src/common/config/configuration.ts
+++ src/common/config/configuration.ts
@@ -2,4 +2,10 @@ export default () => ({
 	port: Number(process.env.PORT) || 4000,
 	apiPrefix: process.env.API_PREFIX || '',
 	nodeEnv: process.env.NODE_ENV || 'production',
+	logLevel: process.env.LOG_LEVEL || 'log',
+	logMaxFileSize: process.env.LOG_MAX_FILE_SIZE
+		? Number(process.env.LOG_MAX_FILE_SIZE) * 1024
+		: 1024 * 1024,
+	logDir: 'logs',
+	logFile: 'app.log',
 });
