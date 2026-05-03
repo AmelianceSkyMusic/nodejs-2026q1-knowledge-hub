56dc7789f01d259b6c2b9c6d2a5dfc0564ebaf87
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-05-03 19:16:33 +0300

    test: add  x-test-mode header to e2e request for skip throttler

--- test/lib/request.ts
+++ test/lib/request.ts
@@ -6,4 +6,12 @@ const port = process.env.PORT || 4000;
 const host = `localhost:${port}`;
 const _request = request(host);
 
-export default _request;
+const testRequest = {
+	get: (url: string) => _request.get(url).set('x-test-mode', 'true'),
+	post: (url: string) => _request.post(url).set('x-test-mode', 'true'),
+	put: (url: string) => _request.put(url).set('x-test-mode', 'true'),
+	delete: (url: string) => _request.delete(url).set('x-test-mode', 'true'),
+	patch: (url: string) => _request.patch(url).set('x-test-mode', 'true'),
+};
+
+export default testRequest;
